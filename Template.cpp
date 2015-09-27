4dd3879b08ab3c0554494840c633bc6b2ee0fba2
tobias <s1190170@gmail.com>
2015-09-27 15:00:19 +0900

    add Template

---
+++ Template.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#define REP(i, n) for (int i=0;i<int(n);++i)
+#define FOR(i, a, b) for (int i=int(a);i<int(b);++i)
+#define DWN(i, b, a) for (int i=int(b-1);i>=int(a);--i)
+#define DO(n) int n____ = n; while(n____--)
+#define TO(i, a, b) int s_=a<b?1:-1,b_=b+s_;for(int i=a;i!=b_;i+=s_)
+#define SQZ(i, j, a, b) for (int i=int(a),j=int(b)-1;i<j;++i,--j)
+#define REP2(i, j, n, m) REP(i, n) REP(j, m)
+
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define INS(A, P, B) A.insert(A.begin() + P, B)
+#define ERS(A, P) A.erase(A.begin() + P)
+#define BSC(A, X) find(ALL(A), X) // != A.end()
+#define CTN(T, x) (T.find(x) != T.end())
+#define SZ(A) int(A.size())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+
+#define Rush int T____; RD(T____); DO(T____)
+#pragma comment(linker, "/STACK:36777216")
+#pragma GCC optimize ("O2")
+
+
+/** Add - On **/
+
+// <<= ` 0. Daily Use .,
+
+template<class T> inline void checkMin(T &a,const T b){if (b<a) a=b;}
+template<class T> inline void checkMax(T &a,const T b){if (b>a) a=b;}
+template <class T, class C> inline void checkMin(T& a, const T b, C c){if (c(b,a)) a = b;}
+template <class T, class C> inline void checkMax(T& a, const T b, C c){if (c(a,b)) a = b;}
+template<class T> inline T min(T a, T b, T c){return min(min(a, b), c);}
+template<class T> inline T max(T a, T b, T c){return max(max(a, b), c);}
+template<class T> inline T sqr(T a){return a*a;}
+template<class T> inline T cub(T a){return a*a*a;}
+int Ceil(int x, int y){return (x - 1) / y + 1;}
+
+
+// <<= ` 1. Bitwise Operation .,
+
+inline bool _1(int x, int i){return x & 1<<i;}
+inline int _1(int i){return 1<<i;}
+inline int _U(int i){return _1(i) - 1;};
+
+inline int count_bits(int x){
+    x = (x & 0x55555555) + ((x & 0xaaaaaaaa) >> 1);
+    x = (x & 0x33333333) + ((x & 0xcccccccc) >> 2);
+    x = (x & 0x0f0f0f0f) + ((x & 0xf0f0f0f0) >> 4);
+    x = (x & 0x00ff00ff) + ((x & 0xff00ff00) >> 8);
+    x = (x & 0x0000ffff) + ((x & 0xffff0000) >> 16);
+    return x;
+}
+
+template<class T> inline T low_bit(T x) {
+    return x & -x;
+}
+
+template<class T> inline T high_bit(T x) {
+    T p = low_bit(x);
+    while (p != x) x -= p, p = low_bit(x);
+    return p;
+}
+
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(false);
+
+
+  
+  return 0;
+}
