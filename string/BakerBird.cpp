f9816834f0735c4401b50de846a272a795ff1eb2
tobias <s1190170@gmail.com>
2015-10-06 18:00:36 +0900

    bug fixed

--- string/BakerBird.cpp
+++ string/BakerBird.cpp
@@ -63,36 +63,46 @@ struct AhoCorasick {
   } 
 };
 
-int bakerBird(const vector<string>&T, const vector<string>&pattern){
-  int pl=pattern.size(),tl=T.size(),cnt=0;
+typedef pair<int,int> pii;
+
+vector<pii> bakerBird(const vector<string>&T, const vector<string>&pattern){
+  int pl=pattern.size(),tl=T.size(),pil=pattern[0].size();
   AhoCorasick aho(1000);
+  
   for(int i=0;i<pl;i++)aho.addString(pattern[i]);
   string acc;
+  
   for(int i=0;i<pl;i++){
-    int node = 0, pil = pattern[i].size();
+    int node = 0;
     for(int j=0;j<pil;j++){
       node = aho.transition(node, pattern[i][j]);
       if(aho.nodes[node].leaf)acc+=(node+'0');
     }
   }
-
-  vector<string>td(tl);
+  
+  const int til = T[0].size();
+    
+  vector<string>td(til);
   for(int i=0;i<tl;i++){
-    int node = 0, til = T[i].size();
+    int node = 0;
     for(int j=0;j<til;j++){
       node = aho.transition(node, T[i][j]);
       td[til-j-1]+=(node+'0');
-    }  
+    }
   }
 
-  for(int i=0;i<tl;i++){
+  vector<pii>res;
+  int tl2=acc.size(),sl=acc.size()+tl+1;
+  vector<int>a(acc.size()+tl+2,-1);
+    
+  for(int i=0;i<til;i++){
     string s=acc+"\1"+td[i];
-    int sl=s.size(),al=acc.size();
-    vector<int>a(s.size(),-1);
-    for(int j=0,k=-1;j<sl;a[++j]=++k)while(k>=0 && s[j]!=s[k])k=a[k];
-    for(int j=tl+1;j<=sl;j++)cnt+=(a[j]==al);
+    for(int k=0,j=-1;k<sl;a[++k]=++j)while(j>=0 && s[k]!=s[j])j=a[j];
+    for(int k=tl2+1;k<=sl;k++){
+      if(a[k]==tl2)res.push_back(pii(k-tl2*2-1,til-i-pil));
+    }
   }
-  return cnt;
+  return res;
 }
 
 int main(void){
@@ -102,7 +112,10 @@ int main(void){
   for(int i=0;i<n;i++)cin >> s[i];
   for(int i=0;i<m;i++)cin >> t[i];
 
-  cout << bakerBird(t,s) << endl;
-  
+  vector<pii> res=bakerBird(t,s);
+
+  for(int i=0;i<res.size();i++)
+    cout << res[i].first << " " << res[i].second << endl;
+      
   return 0;
 }
