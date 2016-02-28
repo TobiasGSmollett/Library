2dc5a7904727e696294da72e1c13e3861e727879
tobias <s1190170@gmail.com>
2016-02-29 01:52:08 +0900

    verified LinkCutTreeArray2

---
+++ experimental/LinkCutTreeArray2.cpp
@@ -0,0 +1,205 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cassert>
+
+#define INF (1<<29)
+
+using namespace std;
+
+struct LinkCutTree {
+  vector<int>left,right,parent,val,mini,minId,lazy;
+  vector<bool>rev;
+  
+  LinkCutTree(int n):left(n,-1),right(n,-1),parent(n,-1),val(n,0),
+		     mini(n,INF),minId(n,-1),lazy(n,0),rev(n,false){}
+
+  void update(int id){
+    mini[id]=val[id],minId[id]=id;
+    push(id);
+    if(left[id]>=0)push(left[id]);
+    if(right[id]>=0)push(right[id]);
+    
+    if(left[id]>=0 && mini[id]>mini[left[id]]){
+      mini[id] = mini[left[id]];
+      minId[id] = minId[left[id]];
+    }
+    if(right[id]>=0 && mini[id]>mini[right[id]]){
+      mini[id] = mini[right[id]];
+      minId[id] = minId[right[id]];
+    }
+  }
+   
+  void push(int id){
+    if(rev[id]){
+      rev[id]=false;
+      swap(left[id],right[id]);
+      if(left[id]>=0)rev[left[id]]=!rev[left[id]];
+      if(right[id]>=0)rev[right[id]]=!rev[right[id]];
+    }
+    if(left[id])lazy[left[id]]+=lazy[id];
+    if(right[id])lazy[right[id]]+=lazy[id];
+
+    val[id]+=lazy[id],mini[id]+=lazy[id],lazy[id]=0;
+  }
+  
+  bool is_root(int id){
+    return parent[id]<0 || (left[parent[id]]!=id && right[parent[id]]!=id);
+  }
+
+  void connect(int ch, int p, bool isL){
+    (isL ? left : right)[p] = ch;
+    if(ch >= 0)parent[ch] = p;
+  }
+
+  void rotate(int id){
+    int p = parent[id], q = parent[p];
+    push(p),push(id);
+    bool isL = id==left[p], isRoot = is_root(p);
+    connect((isL ? right : left)[id], p, isL);
+    connect(p, id, !isL);
+    if(!isRoot)connect(id,q,p==left[q]);
+    else parent[id]=q;
+    update(p);
+  }
+
+  void splay(int id){
+    while(!is_root(id)){
+      int p = parent[id];
+      push(p),push(id);
+      if(!is_root(p))rotate( (id==left[p])^(p==left[parent[p]]) ? p : id );
+      rotate(id);
+    }
+    push(id),update(id);
+  }
+
+  int expose(int id){
+    int last = -1;
+    for(int y=id; y>=0; y=parent[y]) splay(y), left[y]=last, last=y;
+    splay(id);
+    return last;
+  }
+
+  //slow
+  int find_root(int id){
+    expose(id);
+    while(right[id]!=-1)id = right[id];
+    return id;
+  }
+  
+  bool is_connected(int x, int y){
+    if(x == y)return true;
+    expose(x), expose(y);
+    return (parent[x] != -1);
+  }
+
+  void evert(int p){
+    expose(p);
+    rev[p]=!rev[p];
+  }
+
+  void link(int ch, int p){
+    evert(ch);
+    parent[ch]=p;
+  }
+
+  void cut(int id){
+    expose(id);
+    parent[right[id]]=-1;
+    right[id]=-1;
+  }
+
+  int lca(int ch, int p){
+    expose(ch);
+    return expose(p);
+  }
+  int getMinId(int id){
+    expose(id);
+    return minId[id];
+  }
+  int min(int from, int to){
+    evert(from);
+    expose(to);
+    return mini[to];
+  }
+  void add(int id, int val){
+    expose(id);
+    lazy[id]=val;
+  }
+  void add(int from, int to, int v){
+    evert(from);
+    expose(to);
+    lazy[to]+=v;
+  }
+};
+
+
+bool tree[51][51];
+
+bool getPath(int u,int v,int p,vector<int> &path,int n){
+  path.push_back(u);
+  if(u==v)return true;
+  for(int i=0;i<n;i++)
+    if(i!=p && tree[u][i] && getPath(i,v,u,path,n))return true;
+  path.pop_back();
+  return false;
+}
+
+void test(){  
+  srand(time(NULL));
+
+  for(int tc=0;tc<1000;tc++){
+    int n=abs(rand())%50+1,val[n];
+
+    fill(tree[0],tree[50],0);
+    fill(val,val+n,0);
+    
+    LinkCutTree lctree(n);
+    
+    for(int q=0;q<1000;q++){
+      int com=abs(rand())%3;
+      int u=abs(rand())%n,v=abs(rand())%n;
+      if(com==0){
+	lctree.evert(u);
+	lctree.expose(v);
+	if (lctree.left[v] == u && lctree.left[u] == -1 && lctree.right[u] == -1) {
+	  lctree.cut(v);
+	  tree[u][v] = tree[v][u] = false;
+	}
+      }
+      else if(com==1){
+	if(lctree.is_connected(u,v)){
+	  vector<int>path;
+	  getPath(u,v,-1,path,n);
+	  int mini = INF;
+	  for(int i=0;i<path.size();i++)mini=min(mini,val[path[i]]);
+
+	  if(lctree.min(u,v)!=mini){
+	    cout << "assert bitween " << u << " " << v  << " min(u,v) " << lctree.min(u,v) << " mini " << mini << " path.size() "<<path.size()<< endl;
+	    assert(false);
+	  }
+	}
+      }
+      else if(com==2){
+	if(lctree.is_connected(u,v)){
+	  vector<int>path;
+	  getPath(u,v,-1,path,n);
+	  int lazy = abs(rand())%100+1;
+	  for(int i=0;i<path.size();i++)val[path[i]]+=lazy;
+	  lctree.add(u,v,lazy);
+	}
+      }
+      else {
+	if(!lctree.is_connected(u,v)){
+	  //cout <<"connect "<< x->id << " " << y->id << endl;
+	  lctree.link(u,v);
+	  tree[u][v]=tree[v][u]=true;
+	}
+      }
+    }
+  }
+}
+
+int main(void){
+  test();
+}
---
+++ experimental/MaxFlowLinkCutArray.cpp
@@ -0,0 +1,237 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+#define INF (1<<29)
+#define max_n 10000
+
+using namespace std;
+
+struct edge{int to,cap,rev;};
+
+struct LinkCutTree {
+  vector<int>left,right,parent,val,mini,minId,lazy;
+  vector<edge *>edges;
+  LinkCutTree(int n):left(n,-1),right(n,-1),parent(n,-1),val(n,0),
+		     mini(n,INF),minId(n,-1),lazy(n,0),edges(n){}
+  
+  LinkCutTree(int n,int v):left(n,-1),right(n,-1),parent(n,-1),val(n,v),
+			   mini(n,INF),minId(n,-1),lazy(n,0),edges(n,NULL){}
+  
+  void update(int id){
+    mini[id]=val[id],minId[id]=id;
+    
+    if(left[id]>=0 && mini[id]>mini[left[id]]){
+      mini[id] = mini[left[id]];
+      minId[id] = minId[left[id]];
+    }
+    if(right[id]>=0 && mini[id]>=mini[right[id]]){
+      mini[id] = mini[right[id]];
+      minId[id] = minId[right[id]];
+    }
+  }
+  
+  void apply(int id, int v){ lazy[id]+=v, val[id]+=v, mini[id]+=v; }
+  
+  void push(int id){
+    if(left[id]>=0)apply(left[id],lazy[left[id]]);
+    if(right[id]>=0)apply(right[id],lazy[right[id]]);
+    lazy[id]=0;
+  }
+  
+  bool is_root(int id){
+    return parent[id]<0 || (left[parent[id]]!=id && right[parent[id]]!=id);
+  }
+
+  void connect(int ch, int p, bool isL){
+    (isL ? left : right)[p] = ch;
+    if(ch >= 0)parent[ch] = p;
+  }
+
+  void rotate(int id){
+    int p = parent[id], q = parent[p];
+    push(p),push(id);
+    
+    bool isL = id==left[p], isRoot = is_root(p);
+
+    connect((isL ? right : left)[id], p, isL);
+    connect(p, id, !isL);
+
+    if(!isRoot)connect(id,q,p==left[q]);
+    else parent[id]=q;
+
+    update(p);
+  }
+
+  void splay(int id){
+    while(!is_root(id)){
+      int p = parent[id];
+      if(!is_root(p))rotate( (id==left[p])^(p==left[parent[p]]) ? p : id );
+      rotate(id);
+    }
+    push(id),update(id);
+  }
+
+  int expose(int id){
+    int last = -1;
+    for(int y=id; y>=0; y=parent[y])
+      splay(y), left[y]=last, last=y;
+    splay(id);
+    return last;
+  }
+
+  //slow
+  int find_root(int id){
+    expose(id);
+    while(right[id]!=-1)id = right[id];
+    return id;
+  }
+  
+  void link(int ch, int p){
+    expose(ch);
+    expose(p);
+    if(right[ch]>=0)return;
+    parent[ch]=p;
+    left[p]=ch;
+  }
+
+  void link(int c,int p,int v,edge *e){
+    link(c,p);
+    val[c]=v;
+    update(c);
+    edges[c]=e;
+  }
+  
+  void cut(int id){
+    expose(id);
+    if(right[id]<0)return;
+    parent[right[id]]=-1;
+    right[id]=-1;
+
+    val[id]=INF;
+  }
+
+  int lca(int ch, int p){
+    expose(ch);
+    return expose(p);
+  }
+  int getMinId(int id){
+    expose(id);
+    return minId[id];
+  }
+  void add(int id, int val){
+    expose(id);
+    apply(id,val);
+  }
+};
+
+vector<edge> g[max_n];
+
+void add_edge(int from,int to,int cap){
+  g[from].push_back((edge){to,cap,(int)g[to].size()});
+  g[to].push_back((edge){from,0,(int)g[from].size()-1});
+}
+ 
+int dist[max_n];
+
+bool bfs(int s,int t){
+  fill(dist,dist+max_n,-1);
+  dist[s]=0;
+  queue<int>que;
+  que.push(s);
+  while(!que.empty()){
+    int u=que.front();que.pop();
+    if(u==t)return true;
+    for(int j=0;j<g[u].size();j++){
+      edge e=g[u][j];
+      if(dist[e.to]<0 && e.cap>0){
+	dist[e.to]=dist[u]+1;
+	que.push(e.to);
+      }
+    }
+  }
+  return false;
+}
+
+int n,ptr[max_n];
+vector<int>lists[max_n];
+
+bool pour(int id,int i,LinkCutTree* tree){
+  int u=lists[id][i];
+  if(tree->find_root(u)==u)return true;
+  edge *e = tree->edges[u];
+  tree->expose(u);
+  int df=e->cap - tree->val[u];
+  e->cap -= df;
+  g[e->to][e->rev].cap += df;
+  return false;
+}
+
+int max_flow(int s,int t){
+  int flow=0;
+  while(bfs(s,t)){
+    fill(ptr,ptr+max_n,0);
+    LinkCutTree tree(n,INF);
+    for(int i=0;i<n;i++)lists[i].clear();
+    while(true){
+      int v = tree.find_root(s);
+      //cout << tree.getMinId(s) << endl;
+      if(v==t){
+	v=tree.getMinId(s);
+	tree.expose(v);
+	//expose(v=nodes[minId(s)]);
+	flow += tree.mini[v];
+	//flow+=v->mini;
+	tree.add(s,-tree.mini[v]);
+	while(true){
+	  v=tree.getMinId(s);
+	  tree.expose(v);
+	  //expose(v=nodes[minId(s)]);
+	  if(tree.val[v]>0)break;
+	  g[tree.edges[v]->to][tree.edges[v]->rev].cap += tree.edges[v]->cap;
+	  tree.edges[v]->cap = 0;
+	  tree.cut(v);
+	}
+	continue;
+      }
+
+      if(ptr[v] < g[v].size()){
+	edge &e=g[v][ptr[v]++];
+	if(dist[e.to]==dist[v]+1 && e.cap>0){
+	  tree.link(v,e.to,e.cap,&e);
+	  lists[e.to].push_back(v);
+	} 
+      } else {
+	if(v==s){
+	  for(int i=0;i<n;i++){
+	    for(int j=0;j<lists[i].size();j++)pour(i,j,&tree);
+	    lists[i].clear();
+	  }
+	  break;
+	}
+	for(int i=0;i<lists[v].size();i++){
+	  if(!pour(v,i,&tree))tree.cut(lists[v][i]);
+	}
+	lists[v].clear();
+      }     
+    }
+  }
+  return flow;
+}
+
+// AOJ GRL_6_A
+int main(void){
+  
+  int e;
+  cin >> n >> e;
+  for(int i=0;i<e;i++){
+    int a,b,c;
+    cin >> a >> b >> c;
+    add_edge(a,b,c);
+  }
+ 
+  cout << max_flow(0,n-1) << endl;;
+ 
+  return 0;
+}
